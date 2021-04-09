d28945fa34b37615164ece9bab2331e4c2d94379
Mapala1965 <mapalamegha@gmail.com>
2021-04-09 09:47:15 -0400

    tt

---
+++ src/loop/For_Loop.java
@@ -0,0 +1,16 @@
+package loop;
+
+public class For_Loop {
+
+	public static void main(String[] args) {
+		
+		String student [] = {"Dan","Kim","Marshall","Sam","Megan"};
+		
+		for(int i=0; i<5; i++)
+		{
+			System.out.println(student[i]);
+		}
+
+	}
+
+}
